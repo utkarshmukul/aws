848ee3e94f51a6cfa32f117a366e20476bf17331
root <root@ip-172-31-44-178.ap-south-1.compute.internal>
2026-05-20 19:23:27 +0000

    Added separate car_rest_api and bike_rest_api

---
+++ bike_rest_api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"encoding/json"
+
+	"github.com/gorilla/mux"
+	"bike_rest_api/handler"
+)
+
+func printUmv(w http.ResponseWriter, r *http.Request) {
+	log.Println("Request received")
+	// Set the content type to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Define the data to be returned
+	response := map[string]string{"message": "hello world"}
+
+	// Encode and write the response
+	json.NewEncoder(w).Encode(response)
+}
+
+
+func main() {
+	router := mux.NewRouter()
+
+	h := handler.InitBike()
+
+	router.HandleFunc("/bikes", h.CreateBike).Methods("POST")
+	router.HandleFunc("/bikes", h.GetBikes).Methods("GET")
+	router.HandleFunc("/bikes/{id}", h.GetBike).Methods("GET")
+	router.HandleFunc("/bikes/{id}", h.UpdateBike).Methods("PUT")
+	router.HandleFunc("/bikes/{id}", h.DeleteBike).Methods("DELETE")
+	router.HandleFunc("/umv", printUmv).Methods("GET")
+
+	log.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
